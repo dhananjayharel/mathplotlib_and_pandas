a81ca0e60c12bd88d8875bd44189da38948b6bca
dhananjay harel <dj@programmr.com>
2019-12-12 21:59:14 +0530

    code correction

--- machinelearning/bbc_headlines/bbc_headlines.py
+++ machinelearning/bbc_headlines/bbc_headlines.py
@@ -3,9 +3,9 @@ from nltk.corpus import stopwords
 from collections import defaultdict
 from string import punctuation
 from heapq import nlargest
-import urllib2
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
+import nltk
 #pip install nltk
 #pip install bs4
 
@@ -54,19 +54,19 @@ class FrequencySummarizer:
 
 def get_only_text(url):
     #return the title and text of the article at the specified url
-    page = urllib2.urlopen(url).read().decode('utf8')
+    page = urlopen(url).read().decode('utf8')
     soup = BeautifulSoup(page)
     text = ' '.join(map(lambda p: p.text, soup.find_all('p')))
     return soup.title.text, text
 
-feed_xml = urllib2.urlopen('http://feeds.bbci.co.uk/news/rss.xml').read()
+feed_xml = urlopen('http://feeds.bbci.co.uk/news/rss.xml').read()
 feed = BeautifulSoup(feed_xml.decode('utf8'))
 to_summarize = map(lambda p: p.text, feed.find_all('guid'))
 
 fs = FrequencySummarizer()
 for article_url in to_summarize[:5]:
     title, text = get_only_text(article_url)
-    print '------------------------------'
-    print title
+    print('------------------------------')
+    print(title)
     for s in fs.summarize(text, 2):
-        print '*',s 
+        print('*',s) 
