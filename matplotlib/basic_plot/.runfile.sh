5ff2e29b43487d994906ef05a3351535b625a19a
dhananjay <harel.dhananjay@gmail.com>
2019-12-27 15:59:07 +0530

    basic example added

---
+++ matplotlib/basic_plot/.runfile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Running.."
+python3 basic_plot.py
+echo "done"
---
+++ matplotlib/basic_plot/basic_plot.py
@@ -0,0 +1,9 @@
+import random
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4,5], [4,5,6,7,3])
+
+#plt.show()
+plt.savefig('plot.png')
+print('Plot saved in `plot.png`. Please click on it to see the graph.')
+
